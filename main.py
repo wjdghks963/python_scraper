843d60c5e4bfe2d46623a66d2262aff8e574567c
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-03-24 23:39:28 +0900

    Initial Commit

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
 
-indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50")
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
-pagination = indeed_soup.find("div", {"class": "pagination"})
+last_indeed_page = extract_indeed_pages()
 
-pages = pagination.find_all('a')
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
-spans = spans[:-1]
-
+extract_indeed_jobs(last_indeed_page)
