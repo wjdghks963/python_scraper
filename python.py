8308c0937e9203103c34bcf377d290fb2240f108
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-03-26 18:59:18 +0900

    Initial Commit

--- python.py
+++ python.py
@@ -59,6 +59,55 @@ for d in days:
         print(d)
 
 
+
+
+# class, *args, **kwargs, super
+
+
+# def plus(*args):
+#     result = 0
+#     for number in args:
+#         result += number
+#     print(result)
+#
+# plus(1, 2, 1, 1, 1, 2, 1)
+
+
+class Car:
+
+    def __init__(self, *args, **kwargs):
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.seats = 4
+        self.color = kwargs.get("color", "black")
+        self.price = kwargs.get("price", "$20")
+
+    def __str__(self):  # method
+        return f"Car with {self.wheels} wheels"
+
+
+class Convertible(Car):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  # super == 부모로부터 상속
+        self.time = kwargs.get("time", 10)
+
+    def take_off(self):
+        return "taking off"
+
+    def __str__(self):  # method
+        return f"Car with no roof"
+
+
+porche = Convertible(color="green", price="$40")  # porche = instance
+porche.take_off
+print(porche.color, porche.price, porche)
+
+mini = Car()
+print(mini.color, mini.price)
+
+
 # module
 
 # from math import ceil
